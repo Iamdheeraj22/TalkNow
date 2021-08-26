2903c94d71df634388ee0ff2212152ce0adba6ab
Iamdheeraj21 <dheerajprajapati334@gmail.com>
2021-08-26 15:40:07 +0530

    Design the background of tablayout and set.

--- app/src/main/java/com/example/tallnow/Activities/MainActivity.java
+++ app/src/main/java/com/example/tallnow/Activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tallnow.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -12,10 +13,11 @@ import androidx.viewpager.widget.ViewPager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.tallnow.Classes.Chat;
@@ -24,6 +26,7 @@ import com.example.tallnow.Fragments.Chats;
 import com.example.tallnow.Fragments.Groups;
 import com.example.tallnow.Fragments.Users;
 import com.example.tallnow.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -43,36 +46,35 @@ public class MainActivity extends AppCompatActivity {
     TextView uname;
     Toolbar toolbar;
     FirebaseUser firebaseUser;
+    ImageView logoutImage;
+    //Handler handler;
     DatabaseReference databaseReference;
-
+    FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User user=snapshot.getValue(User.class);
-                assert user != null;
-                uname.setText(user.getUsername());
-                if(user.getImageurl().equals("default"))
-                {
-                    profile.setImageResource(R.mipmap.ic_launcher_round);
-                }else {
-                    Glide.with(getApplicationContext()).load(user.getImageurl()).into(profile);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
+        getAndSetInfo();
         final TabLayout tabLayout=findViewById(R.id.tab_layout);
         final ViewPager viewPager=findViewById(R.id.view_pager);
 
+        floatingActionButton.setOnClickListener(v->
+                startActivity(new Intent(getApplicationContext(), User_Profile_Activity.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK)));
+        setMessageAndTablayout(tabLayout,viewPager);
+        logoutImage.setOnClickListener(v -> {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Alert");
+            alert.setPositiveButton("Yes", (dialog, which) -> {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Start_Activity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }).setNegativeButton("No",null);
+            alert.show();
+        });
+    }
+    private void setMessageAndTablayout (TabLayout tabLayout, ViewPager viewPager)
+    {
         databaseReference=FirebaseDatabase.getInstance().getReference("Chats");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -96,20 +98,21 @@ public class MainActivity extends AppCompatActivity {
                 viewPager.setAdapter(viewPagerAdapter);
                 tabLayout.setupWithViewPager(viewPager);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void initViews ()
     {
-        profile=findViewById(R.id.profile_pic1);
-        uname=findViewById(R.id.uname2);
+        //handler=new Handler();
         toolbar=findViewById(R.id.toolbar);
-        toolbar.inflateMenu(R.menu.main_menu);
+        profile=toolbar.findViewById(R.id.profile_pic1);
+        uname=toolbar.findViewById(R.id.uname2);
+        logoutImage=toolbar.findViewById(R.id.logoutButton);
+        floatingActionButton=findViewById(R.id.floatingButton1);
         firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
         databaseReference= FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
     }
@@ -124,10 +127,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onContextItemSelected (@NonNull MenuItem item) {
         if (item.getItemId() == R.id.logout) {
             FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(getApplicationContext(), Start.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            startActivity(new Intent(getApplicationContext(), Start_Activity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             return true;
         }else if(item.getItemId()==R.id.Profile){
-            startActivity(new Intent(getApplicationContext(),User_Profile.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            startActivity(new Intent(getApplicationContext(), User_Profile_Activity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             return true;
         }else if (item.getItemId() == R.id.create_group){
             startActivity(new Intent(MainActivity.this,CreateGroup_Activity.class));
@@ -135,21 +138,6 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    /*@Override
-    public boolean on(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.logout) {
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(getApplicationContext(), Start.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-            return true;
-        }else if(item.getItemId()==R.id.Profile){
-            startActivity(new Intent(getApplicationContext(),User_Profile.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-            return true;
-        }else if (item.getItemId() == R.id.create_group){
-            startActivity(new Intent(MainActivity.this,CreateGroup_Activity.class));
-            return true;
-        }
-        return false;
-    }*/
 
     public static class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
@@ -178,7 +166,6 @@ public class MainActivity extends AppCompatActivity {
             fragments.add(fragment);
             titles.add(title);
         }
-
         @Nullable
         @Override
         public CharSequence getPageTitle(int position) {
@@ -203,4 +190,28 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         status("Offline");
     }
+
+    //Get and Set the Image and name
+    private void getAndSetInfo(){
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user=snapshot.getValue(User.class);
+                assert user != null;
+                uname.setText(user.getUsername());
+                if(user.getImageurl().equals("default"))
+                {
+                    profile.setImageResource(R.mipmap.ic_launcher_round);
+                }else {
+                    Glide.with(getApplicationContext()).load(user.getImageurl()).into(profile);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }
